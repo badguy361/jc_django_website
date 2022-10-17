1bddcc3f1313b21e8bc81bbda6c4a3dbdec064c3
joey <t1616joy@yahoo.com.tw>
2022-10-17 21:26:27 +0800

    remove en page and debug=false

--- jc_website/settings.py
+++ jc_website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(0huu$7g10xk1@$vfdkf@yxy(a6e%$dy+fks#t9n7fnlmb024i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.100','*']
 CSRF_TRUSTED_ORIGINS = ["https://cye-dream.azurewebsites.net"]
